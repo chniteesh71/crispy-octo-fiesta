e275d6156144cff9784f673748cafdb7779a3d70
chniteesh71 <chniteesh71@gmail.com>
2025-10-01 15:31:18 +0530

    commit-4

---
+++ src/main/java/com/yourname/flappybird/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the Flappy Bird clone game implemented in JavaFX.
+ * It includes the main game logic, rendering, and input handling.
+ */
+package com.yourname.flappybird;
